764a9492174fa918bd0ab75140dc09676ed866c2
Taly96 <talicht96@gmail.com>
2023-04-21 21:50:13 +0300

    update

--- TalyTochner.java
+++ TalyTochner.java
@@ -1,7 +1,7 @@
 public class TalyTochner{
 	public static void main(String[] args) {
 		for(int i = 1; i <= 10; i++) {
-			System.out.println("Taly Tochner" +i);
+			System.out.println("Tochner Taly" +i);
 		}
 	}
 }
